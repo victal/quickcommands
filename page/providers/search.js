f4f484f49c47135af4545fad983ad0948247fc20
Guilherme Victal <guilhermevictal@gmail.com>
2020-10-05 19:44:31 -0300

    First implementaton of search entry

---
+++ page/providers/search.js
@@ -0,0 +1,19 @@
+/* global closePopup,Search,TabList */
+const updateSearch = async (filterText) => {
+  if (!filterText) {
+    return []
+  }
+  const searchEngines = await browser.search.get()
+  if (searchEngines.length === 0) {
+    return []
+  }
+  const defaultEngines = searchEngines.filter(engine => engine.isDefault)
+  let defaultEngine = searchEngines[0]
+  if (defaultEngines.length > 0) {
+    defaultEngine = defaultEngines[0]
+  }
+  return [new Search(defaultEngine, filterText, closePopup)]
+}
+
+/* exported searchList */
+const searchList = new TabList('', updateSearch)
